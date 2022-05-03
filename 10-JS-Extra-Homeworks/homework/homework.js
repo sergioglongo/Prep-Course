475b2a70547bd3917d1800fa234fe64911dc0d32
unknown <sergiolongo@gmail.com>
2022-05-03 16:12:03 -0300

    2 ejercicios resueltos

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  resultado=[];
+  let i =0;
+    for(const propiedad in objeto){
+      resultado[i++]=[propiedad,objeto[propiedad]];
+    }
+ return resultado;
 }
 
 
@@ -18,6 +24,23 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+let resultado={};
+let cont=1;
+for(let i=0;i< string.length;i++)
+{
+
+  for(let j=0;j< string.length;j++)
+  {
+    if(string[i] == string[j])
+    {
+      resultado[string[i]]=null;
+      resultado[string[i]]=cont++;
+    }
+  }
+  cont=1;
+}
+return resultado;
 }
 
 
